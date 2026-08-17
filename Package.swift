24f7f02657e242c7120a7e0d39f146febcac3cf8
Qehreman <kerimovq5@gmail.com>
2026-08-17 12:35:26 +0400

    fixed Package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "SilentMoonNetwork",
     platforms: [.iOS(.v16)],
     products: [
-        .library(name: "SilentMoonNetwork",
-                 targets: ["SilentMoonNetwork"]),
+        .library(
+            name: "SilentMoonNetwork",
+            targets: ["SilentMoonNetwork"]
+        ),
     ],
     targets: [
         .target(
